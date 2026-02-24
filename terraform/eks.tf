1a735e21862a3881b7f712a89408617628aa9a90
sarthak bordia <sarthakbordia@sarthaks-MacBook-Air.local>
2026-02-24 21:40:23 +0530

    sedond commit

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -8,9 +8,13 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # IMPORTANT FIX
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.small"]
       desired_size   = 1
       min_size       = 1
       max_size       = 1
